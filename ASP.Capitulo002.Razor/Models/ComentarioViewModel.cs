5e7d1b566f152eefc79c85111837ac1806d066ff
Matteus Barbosa <matteus.barbosa@yahoo.com.br>
2023-09-04 00:52:50 -0300

    Adicionar arquivos de projeto.

---
+++ ASP.Capitulo002.Razor/Models/ComentarioViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing.Drawing2D;
+
+namespace ASP.Capitulo002.Razor.Models
+{
+    public class ComentarioViewModel
+    {
+        public DateTime Data { get; set; }
+        public string Nome { get; set; }
+        public string Comentario { get; set; }
+    }
+}
