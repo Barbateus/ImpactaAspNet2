b7dfcc8115deff768fb050c36e2452871bec63e4
Matteus Barbosa <matteus.barbosa@yahoo.com.br>
2023-09-05 03:56:23 -0300

    PrimeiroErrorIIS

--- Marketplace.Mvc/Models/ClienteViewModel.cs
+++ Marketplace.Mvc/Models/ClienteViewModel.cs
@@ -11,15 +11,21 @@ namespace Marketplace.Mvc.Models
         public int Id { get; set; }
 
         [Required]
-        public string Documento { get; set; }
-
-        [Required]
+        [StringLength(60, ErrorMessage = "O Campo Nome pode ter no máximo 60 caracteres.", MinimumLength =10)]
         public string Nome { get; set; }
 
         [Required]
+        [RegularExpression(@"^\d{3}.\d{3}.\d{3}-\d{2}$")]
+        public string Documento { get; set; }
+
+       
+
+        [Required]
+        [StringLength(20, ErrorMessage ="O campo {0} pode ter no máximo {1} caracteres.")]
         public string Telefone { get; set; }
 
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
     }
 }
